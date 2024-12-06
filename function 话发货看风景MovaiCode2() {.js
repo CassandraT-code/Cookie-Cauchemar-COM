0f2ef7065e099b666c853ab78940005b623565d3
Tony GUAN <95930252+GuanTony@users.noreply.github.com>
2024-12-06 06:30:43 +0100

    Add files via upload

---
+++ function 话发货看风景MovaiCode2() {.js
@@ -0,0 +1,70 @@
+// function 话发货看风景MovaiCode2() {
+//     int ;زائل
+    
+//     djdfkqjdk = 1
+//     if (djdfkqjdk != 2){
+//         switch(1){
+//             case 1:
+//                 prout = 1
+//                 break
+//             case 2:
+//                 prout = 2
+//                 break
+//             case 3: 
+//                 prout = 3
+//                 break
+//             case 4: 
+//                 prout = 4
+//                 break
+//             case 5:
+//                 prout = 5
+//                 break
+//             case 6 :
+//                 prout = 6 
+//                 break
+
+//             case 7 : 
+//                 prout = 7
+//                 break
+//             case 8 : 
+//                 prout =8
+//                 break
+//             case 9 : 
+//                 Prout = 9
+//                 break
+//             case 10 : 
+//                 Prouhdjt = 10
+//                 break
+//             case wtf_pk_je_fais_ca = 11 :   
+//                 tkt = 11;
+//                 break
+//             default :
+//                 ekjrkghlfml = 5;
+
+//         }
+//         if ((prout || Prout || tkt || Prouhdjt || 5) != 5) {
+//             prout = 5
+//             Prout =5
+//             tkt   = 5
+//             Prouhdjt    =5
+//         }
+
+
+//     }
+//     if (qjkhdqjksdh % 5 !== 0) {
+//       const sjdhdckqjhfhqhdnfjjzkfjk = button.offsetWidth;
+//       const sjdhdckqjhfhqhdnfjjzkfjkqsdQZRTVGARFAF = button.offsetHeight;
+//       const SQDKjqshcnksdqnbdfjkfqnjkf12367438I = window.innerWidth;
+//       const SQDKjqsboudmca367438I = window.innerHeight;
+  
+//       let fdlkqshfjkzan, djkhflkhjenfinqdf;
+  
+//       do {
+//         fdlkqshfjkzan = Math.random() * (SQDKjqshcnksdqnbdfjkfqnjkf12367438I - sjdhdckqjhfhqhdnfjjzkfjk);
+//         djkhflkhjenfinqdf = Math.random() * (SQDKjqsboudmca367438I - sjdhdckqjhfhqhdnfjjzkfjkqsdQZRTVGARFAF);
+//       } while (false); 
+  
+//       button.style.left = fdlkqshfjkzan + "px";
+//       button.style.top = djkhflkhjenfinqdf + "px";
+//     }
+// }
